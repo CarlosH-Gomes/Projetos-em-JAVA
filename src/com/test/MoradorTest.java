4dfd33bbae79a2373d775cd80cf29c1d927e5db1
Carlos Henrique Gomes <45035393+CarlosH-Gomes@users.noreply.github.com>
2019-09-13 16:55:33 -0300

    Add files via upload

---
+++ src/com/test/MoradorTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.test;
+
+import com.model.bean.ContasMorador;
+import com.model.bean.Morador;
+import com.model.dao.ContasMoradorDAO;
+import com.model.dao.MoradorDAO;
+
+/**
+ *
+ * @author Carlos
+ */
+public class MoradorTest {
+    
+    public static void main(String[] args) {
+        ContasMoradorDAO mdao = new ContasMoradorDAO();
+        ContasMorador mt = new ContasMorador();
+        
+//        Morador m = null;
+//        m.setCpf("47619467840");
+//        mt.setMoradorCpf(m);
+//        mdao.save(mt);
+        
+    }
+    
+}
